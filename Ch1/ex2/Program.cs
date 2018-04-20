3c4e5f3b8091b2dd4d908a143b99f1706e8fc6f6
Edwardkuo <Edward_kuo@kingston.com.tw>
2018-04-20 17:17:47 +0800

    OK

---
+++ Ch1/ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Ex2_1();
+            Ex2_2();
+        }
+        
+        public static void Ex2_1()
+        {
+            Console.WriteLine("12345678901234567890");
+
+            Console.Write("a.{0}",100);
+
+            Console.Write("     b.{0}",-200);
+            Console.WriteLine();
+
+
+            Console.WriteLine("a.{0}     b.{1}",100,-200);
+        }
+
+        public static void Ex2_2()
+        {
+            int no1,no2;
+            Console.Write("輸入第一個整數No1 : ");
+            no1=int.Parse(Console.ReadLine());
+            Console.Write("輸入第二個整數No2 : ");
+            no2=int.Parse(Console.ReadLine());
+            Console.WriteLine($"no1+no2={no1}+{no2}={no1+no2}");
+        }
+    }
+}
---
+++ Ch1/home1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace home1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          homework();
+        }
+
+        public static void homework()
+        {
+            string ItemName;
+            int price=0;
+            Console.Write("請輸入品名 :");
+            ItemName=Console.ReadLine();
+            Console.Write("請輸入單價 :");
+            price=int.Parse(Console.ReadLine());
+            Console.WriteLine($"品名 : {ItemName} , 單價 : {price.ToString()} , 這筆紀錄儲存成功");
+        }
+    }
+}
