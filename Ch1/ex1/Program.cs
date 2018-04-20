b41e294b981c06d611e27032489e6e8c7f20a9bd
Edward Kuo <edward_Kuo@kingston.com.tw>
2018-04-20 16:45:54 +0800

    OK

---
+++ Ch1/ex1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        //todo: https://docs.microsoft.com/zh-tw/dotnet/csharp/language-reference/operators/increment-operator
+        static void Main(string[] args)
+        {
+
+          ex1_1();
+          ex1_2();
+          ex1_3();
+        }
+
+        public static void ex1_1()
+        {
+            int i =10,K;
+            K=i++;
+            Console.WriteLine(K.ToString());
+        }
+        public static void ex1_2()
+        {
+            int i=10,k;
+            k=++i;
+            Console.WriteLine(k.ToString());
+        }
+        public static void ex1_3()
+        {
+            int i,j,k;
+            i=j=10;
+            k=++i*5;
+            Console.WriteLine($"k={k}");
+            j=k++*2;
+            Console.WriteLine($"j={j}");
+        }
+    }
+}
